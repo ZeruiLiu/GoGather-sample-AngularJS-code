29891c13fe3246a25c53d0c375f7011eee3f51e0
ZeruiLiu <zliu1027@gmail.com>
2014-05-12 00:26:26 -0400

    Create FormController.js

---
+++ FormController.js
@@ -0,0 +1,69 @@
+function FormController($scope){
+
+	// Define the model properties. The view will loop
+	// through the services array and genreate a li
+	// element for every one of its items.
+
+	$scope.clubs = [
+		{
+			name: 'ChelseaFC',
+			capacity: 41837,
+			location:'England',
+			active:true
+		},{
+			name: 'Manchester United',
+			capacity: 75731,
+			location:'England',
+			active:true
+		},{
+			name: 'LiverPoolFC',
+			capacity: 45276,
+			location:'England',
+			active:true
+		},{
+			name: 'RealMadrid',
+			capacity: 85454,
+			location:'Spain',
+			active:true
+		},{
+			name: 'FCBarcelona',
+			capacity: 99786,
+			location:'Spain',
+			active:true
+		},{
+			name: 'ACMilan',
+			capacity: 80018,
+			location:'Italy',
+			active:true
+		},{
+			name: 'InterMilan',
+			capacity: 80018,
+			location:'Italy',
+			active:true
+		},{
+			name: 'FC Bayern Munich',
+			capacity: 71137,
+			location:'Germany',
+			active:true
+		}
+	];
+
+	$scope.toggleActive = function(s){
+		s.active = !s.active;
+	};
+
+	
+
+	$scope.total = function(){
+
+		var total = 0;
+
+		angular.forEach($scope.clubs, function(s){
+			if (s.active){
+				total+= s.capacity;
+			}
+		});
+
+		return total;
+	};
+}
